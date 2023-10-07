1b8436f6022305303c1e44983ad33cd359eeebda
iammradarsh <gachhaadarsh122@gmail.com>
2023-10-07 22:40:20 +0530

    expiresIn changes from JWT EXPIRE 20h

--- final1/utils/SendToken.js
+++ final1/utils/SendToken.js
@@ -3,7 +3,9 @@ exports.sendtoken = (employe,statusCode,res)=>{
     const token = employe.getjwttoken();
 
     const options = {
-        expiresIn:'20h',
+        expires:new Date(
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
         httpOnly:true,
     };
 
